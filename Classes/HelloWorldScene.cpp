c0d85a5182609a8aed84d19839b7b3f5d69b9a02
kurita kazuki <kanoya0410@gmail.com>
2019-07-27 14:12:04 +0900

    kadai

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -23,6 +23,7 @@
  ****************************************************************************/
 
 #include "HelloWorldScene.h"
+#include "AudioEngine.h"
 #include "SimpleAudioEngine.h"
 
 USING_NS_CC;
@@ -35,8 +36,8 @@ Scene* HelloWorld::createScene()
 // Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename)
 {
-	printf("Error while loading: %s\n", filename);
-	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+	//printf("Error while loading: %s\n", filename);
+	//printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
 // on "init" you need to initialize your instance
@@ -57,80 +58,126 @@ bool HelloWorld::init()
 	//    you may modify it.
 
 	// add a "close" icon to exit the progress. it's an autorelease object
-	auto closeItem = MenuItemImage::create(
+	/*auto closeItem = MenuItemImage::create(
 		"CloseNormal.png",
 		"CloseSelected.png",
 		CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-
-	if (closeItem == nullptr ||
-		closeItem->getContentSize().width <= 0 ||
-		closeItem->getContentSize().height <= 0)
-	{
-		problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-	}
-	else
-	{
-		float x = origin.x + visibleSize.width - closeItem->getContentSize().width / 2;
-		float y = origin.y + closeItem->getContentSize().height / 2;
-		closeItem->setPosition(Vec2(x, y));
-	}
-
-	// create menu, it's an autorelease object
-	auto menu = Menu::create(closeItem, NULL);
-	menu->setPosition(Vec2::ZERO);
-	this->addChild(menu, 1);
-
-	/////////////////////////////
-	// 3. add your codes below...
-
-	// add a label shows "Hello World"
-	// create and initialize a label
-
-	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-	if (label == nullptr)
-	{
-		problemLoading("'fonts/Marker Felt.ttf'");
-	}
-	else
-	{
-		// position the label on the center of the screen
-		label->setPosition(Vec2(origin.x + visibleSize.width / 2,
-			origin.y + visibleSize.height - label->getContentSize().height));
-
-		// add the label as a child to this layer
-		this->addChild(label, 1);
-	}
+*/
+	
+	//背景
+	Sprite* haikei = Sprite::create("haikei2.png");//宇宙
+	this->addChild(haikei);
+	haikei->setScale(2.5f);
+	haikei->setPosition(visibleSize.width / 2, visibleSize.height / 2);
 
 	// Spriteの生成
-	Sprite* spr = Sprite::create("inu.png");
+	Sprite* spr1 = Sprite::create("Kirby.png");//カービィ
+	this->addChild(spr1);
+	spr1->setScale(0.5f);
+	spr1->setPosition(Vec2(1200.0f, visibleSize.height / 1.5));
+
+	Sprite* spr2 = Sprite::create("muteki.png");//キャンディ
+	this->addChild(spr2);
+	spr2->setScale(0.25);
+	spr2->setPosition(Vec2(980.0f, visibleSize.height / 1.5));
+
+	Sprite* spr = Sprite::create("soul.png");//マルクソウル
 	this->addChild(spr);
-	spr->setPosition(visibleSize.width, visibleSize.height);
-	//spr->removeFromParent();
-	//MoveTo* action1 = MoveTo::create(5.0f,Vec2(visibleSize.width/2,visibleSize.height/2));
-	//FadeOut*action2 = FadeOut::create(5.0f);
-	//Spawn* action3 = Spawn::create(action1, action2, nullptr);
-	//MoveTo* action4 = MoveTo::create(5.0f, Vec2(visibleSize.width, visibleSize.height));
-	//FadeIn* action5 = FadeIn::create(5.0f);
-	//Spawn* action6 = Spawn::create(action4, action5, nullptr);
-	//spr->setVisible(false);
-	// 指定秒数待機するアクションの生成
-	//DelayTime* action1 = DelayTime::create(1.0f);
-	//自分を削除（解放）するアクション
-	//RemoveSelf* action2 = RemoveSelf::create();
+	spr->setScale(2.0f);
+	spr->setPosition(Vec2(100.0f, visibleSize.height / 1.5));
 
-	MoveTo* action1 = MoveTo::create(1.0f, Vec2(0, visibleSize.height));
-	MoveTo* action2 = MoveTo::create(1.0f, Vec2(0, 0));
-	Sequence* action3 = Sequence::create(action1, action2, nullptr);
-	MoveTo* action4 = MoveTo::create(1.0f, Vec2(visibleSize.width, 0));
-	MoveTo* action5 = MoveTo::create(1.0f, Vec2(visibleSize.width, visibleSize.height));
-	Sequence* action6 = Sequence::create(action4, action5, nullptr);
-	Sequence* action7 = Sequence::create(action3, action6, nullptr);
+	Sprite* spr3 = Sprite::create("nova.png");//ギャラクティック・ノヴァ
+	this->addChild(spr3);
+	spr3->setScale(3.0f);
+	spr3->setPosition(Vec2(visibleSize.width / 2, visibleSize.height/2-100));
+
+	Sprite* spr4 = Sprite::create("tobe.png");//ToBeContinued...
+	this->addChild(spr4);
+	spr4->setScale(0.7f);
+	spr4->setPosition(Vec2(visibleSize.width+320.0f, 100.0f));//280 100
+
+	//Spawn 同時アクション
+	//Sequence 順番アクション
+	spr3->setVisible(0);
+
+	//ノヴァアクション
+	DelayTime* novataiki = DelayTime::create(10.0f);
+	Show* novashow = Show::create();
+	Sequence* novalast = Sequence::create(novataiki, novashow, nullptr);
+	//ノヴァ終わり
+
+	//カービィアクション
+	MoveTo* action1 = MoveTo::create(5.0f,Vec2(visibleSize.width/2+50,visibleSize.height/1.5));
+	EaseIn* action4 = EaseIn::create(action1, 2.0f);
+    Hide*hideK = Hide::create();
+	DelayTime*Kdel = DelayTime::create(5.0f);
+	Sequence* Ksp = Sequence::create(Kdel, hideK, nullptr);
+	Sequence*Kact = Sequence::create(action4, Ksp, nullptr);
+
+	DelayTime*dk = DelayTime::create(3.0f);
+	Blink* bk = Blink::create(6.5f, 20);
+	Sequence* kact1 = Sequence::create(dk, bk, nullptr);
+	
+	//マルクアクション
+	MoveTo* Mstart = MoveTo::create(5.0f, Vec2(visibleSize.width / 2-50, visibleSize.height / 1.5));
+	RotateTo* action6 = RotateTo::create(1.0f, -150.0f);
+	MoveTo* Mdown = MoveTo::create(5.0f, Vec2(visibleSize.width / 2 - 50, -100.0f));
+	Spawn* Mds = Spawn::create(action6, Mdown, nullptr);
+	Sequence* Mac1 = Sequence::create(Mstart, Mds, nullptr);
+	Place*Mp = Place::create(Vec2(visibleSize.width / 2, visibleSize.height + 86));
+	Sequence* Mac2 = Sequence::create(Mac1, Mp, nullptr);
+	DelayTime*Md = DelayTime::create(1.0f);
+	MoveTo*Ml = MoveTo::create(1.0f,Vec2(visibleSize.width / 2, visibleSize.height - 50));
+	Sequence* Mac3 = Sequence::create(Mac2, Md, nullptr);
+	Sequence* Mac4 = Sequence::create(Mac3, Ml, nullptr);
+
+	DelayTime*hai = DelayTime::create(12.1f);
+	TintTo*htin = TintTo::create(0.1, 153, 153, 102);
+	Sequence*hact1 = Sequence::create(hai, htin, nullptr);
 	
 
+	//キャンディアクション
+	DelayTime* mutekitaiki = DelayTime::create(3.0f);
+	RemoveSelf* mutekikesi = RemoveSelf::create();
+	Sequence*muteki = Sequence::create(mutekitaiki, mutekikesi, nullptr);
+	//キャンディ終わり
+
+	//Tobeアクション
+	DelayTime* td = DelayTime::create(12.0f);
+	MoveTo* tm = MoveTo::create(0.1f, Vec2(280.0f, 100.0f));
+	Sequence* tact1 = Sequence::create(td, tm, nullptr);
 	
-	RepeatForever* action8 = RepeatForever::create(action7);
+	DelayTime*nai = DelayTime::create(12.1f);
+	TintTo*ntin = TintTo::create(0.1, 153, 153, 102);
+	Sequence*nact1 = Sequence::create(nai, ntin, nullptr);
+	
+	DelayTime*mai = DelayTime::create(12.1f);
+	TintTo*mtin = TintTo::create(0.1, 153, 153, 102);
+	Sequence*mact1 = Sequence::create(mai, mtin, nullptr);
+
+	
+	
+	experimental::AudioEngine::play2d("To.mp3");
+	
+	
 	// アクションの実行
-	spr->runAction(action8);
+	//カービィ 
+	spr1->runAction(Kact);
+	spr1->runAction(kact1);
+	//マルク
+	spr->runAction(Mac4);
+	spr->runAction(mact1);
+	//キャンディ　
+	spr2->runAction(muteki);
+	//ノヴァ　
+	spr3->runAction(novalast);
+	spr3->runAction(nact1);
+	//
+	spr4->runAction(tact1);
+
+	haikei->runAction(hact1);
+	
+
 
 	// update関数を有効にする
 	this->scheduleUpdate();
